c6ea7bba53967fc16281c902ff0df7bb4884cb34
Logan Blinco (eLF) <logancberrypie@live.com>
2017-09-03 21:56:14 +0100

    Fibonarchi File

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(fib(10));
+            Console.ReadLine();
+        }
+
+        static int fib(int max)
+        {
+            if (max == 0)
+            {
+                return 0;        
+            }
+            else if (max == 1)
+            {
+                return 1;
+        
+            }
+            else
+            {
+                return fib(max - 1) + fib(max - 2);   
+            }
+        }
+
+
+    }
+}
